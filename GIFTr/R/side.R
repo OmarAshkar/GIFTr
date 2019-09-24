3b0f1b0bfcb9c46c68983e6c4a1d8f0112ca5cd5
Omar Elashkar <13979040+omarelashkar@users.noreply.github.com>
2019-09-24 22:39:32 +0200

    Delete side.R

--- GIFTr/R/side.R
+++
@@ -1,5 +0,0 @@
-# dat <- readxl::read_xlsx( 'C:\\Users\\omar.ibrahim\\Desktop\\pharmacy practice
-# exam\\Pharmacy Exam pool.xlsx' , 1) GIFTr(data = dat , Questions = 2 , answers = c(3:7)
-# , categories = 1 , Question_Type = 8 , output = 'omar.txt') #datmcq <-
-# datmcq[which(datmcq$Question_Type == 'mcq'), ] mcq( data = datmcq , Questions = 2 ,
-# categories = 1 , answers = c(3:7) , Question_Type = 8 , output = 'mcq1.txt' )
